f587b25da0c07d16c11307c9ef410532abf492e5
dhyanivj <vijay97dhyani@gmail.com>
2023-01-26 16:54:05 +0530

    form complete

--- src/Registration.js
+++ src/Registration.js
@@ -18,65 +18,65 @@ const Registration = () => {
         console.log(values);
       },
     });
-  console.log(errors);
+  // console.log(errors);
 
   return (
     <>
       <form onSubmit={handleSubmit}>
-    <div>
-    <label htmlFor="name">Name</label>
-        <input
-          type="text"
-          id="name"
-          className="input mb-4"
-          name="name"
-          value={values.name}
-          onChange={handleChange}
-          onBlur={handleBlur}
-        />
-        {errors.name && touched.name ? <span className="text-red-800">{errors.name}</span> : null}
-      </div>
-       
-     <div>
-     <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          id="email"
-          className="input mb-4"
-          name="email"
-          value={values.email}
-          onChange={handleChange}
-          onBlur={handleBlur}
-        />
-     </div>
-
-   <div>
-   <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          id="password"
-          className="input mb-4"
-          name="password"
-          value={values.password}
-          onChange={handleChange}
-          onBlur={handleBlur}
-        />
-    </div>
-        
-      <div>
-      <label htmlFor="confirmPassword">Confirm Password</label>
-        <input
-          type="password"
-          id="confirmPassword"
-          className="input mb-4"
-          name="confirmPassword"
-          value={values.confirmPassword}
-          onChange={handleChange}
-          onBlur={handleBlur}
-        />
+        <div className="mb-4">
+          <label htmlFor="name" className="font-semibold">Name</label>
+          <input
+            type="text"
+            id="name"
+            className="input"
+            name="name"
+            value={values.name}
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />
+          {errors.name && touched.name ? <span className="text-red-800">{errors.name}</span> : null}
         </div>
-       
-        <button type="submit" className="btn btn-primary">Submit</button>
+
+        <div className="mb-4">
+          <label htmlFor="email" className="font-semibold">Email</label>
+          <input
+            type="email"
+            id="email"
+            className="input"
+            name="email"
+            value={values.email}
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />
+        </div>
+
+        <div className="mb-4">
+          <label htmlFor="password" className="font-semibold">Password</label>
+          <input
+            type="password"
+            id="password"
+            className="input"
+            name="password"
+            value={values.password}
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />
+        </div>
+
+        <div className="mb-4">
+          <label htmlFor="confirmPassword" className="font-semibold ">Confirm Password</label>
+          <input
+            type="password"
+            id="confirmPassword"
+            className="input"
+            name="confirmPassword"
+            value={values.confirmPassword}
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />
+        </div>
+
+        <button type="submit" className="btn btn-primary font-semibold">Submit</button>
       </form>
     </>
   );
